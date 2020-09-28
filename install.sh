7df86dc3eb624fe43ce67f1f68dc120f850db032
someoneonsmile <someoneonsmile@gmail.com>
2020-09-28 19:17:11 +0800

    add install list

---
+++ install.sh
@@ -0,0 +1,9 @@
+!#/bin/shell
+sudo apt-get update
+sudo apt-get install tmux -y
+sudo apt-get install ncdu -y
+sudo apt-get install fzf -y
+sudo apt-get install ripgrep -y -y
+sudo apt-get install silversearcher-ag -y
+sudo apt-get install jq -y
+sudo apt-get install axel -y
