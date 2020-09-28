9ab946e540a9c45a990a753348e0fb5c2e620ef0
someoneonsmile <someoneonsmile@gmail.com>
2020-09-28 19:55:43 +0800

    安装清单添加注释

--- install.sh
+++ install.sh
@@ -1,9 +1,14 @@
-!#/bin/shell
+#!/bin/bash
 sudo apt-get update
 sudo apt-get install tmux -y
+# 扫描磁盘工具
 sudo apt-get install ncdu -y
+# 模糊查找工具
 sudo apt-get install fzf -y
+# 最快的文本检索工具
 sudo apt-get install ripgrep -y -y
 sudo apt-get install silversearcher-ag -y
+# 命令 json 工具
 sudo apt-get install jq -y
+# 多线程下载工具
 sudo apt-get install axel -y
