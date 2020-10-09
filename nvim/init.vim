1c1ed907907a1253f9c9e74f7fe609c66b575640
someoneonsmile <someoneonsmile@gmail.com>
2020-10-09 19:01:34 +0800

    更改启动安装方式

--- nvim/init.vim
+++ nvim/init.vim
@@ -1,5 +1,5 @@
-set runtimepath+=~/.nvim_runtime
+set runtimepath+=~/.dotFile/nvim
 
-source ~/.nvim_runtime/my_configs.vim
-source ~/.nvim_runtime/my_plugins.vim
-source ~/.nvim_runtime/coc_configs.vim
+source ~/.dotFile/nvim/my_configs.vim
+source ~/.dotFile/nvim/my_plugins.vim
+source ~/.dotFile/nvim/coc_configs.vim
--- nvim/install.sh
+++ nvim/install.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 set -e
 
-echo 'set runtimepath+=~/.nvim_runtime
-source ~/.nvim_runtime/my_configs.vim
-source ~/.nvim_runtime/my_plugins.vim
-source ~/.nvim_runtime/coc_configs.vim
-' > ~/.vimrc
+if [ -f ~/.vimrc ]; then
+    rm -f ~/.vimrc
+fi
+if [ -f ~/.config/nvim/init.vim ]; then
+    rm -f ~/.config/nvim/init.vim
+fi
+if [ -f ~/.config/nvim/coc-settings.json ]; then
+    rm -f ~/.config/nvim/coc-settings.json
+fi
+ln -s `pwd`/init.vim ~/.vimrc
+ln -s `pwd`/init.vim ~/.config/nvim/init.vim
+ln -s `pwd`/coc-settings.json ~/.config/nvim/coc-settings.json
 
 echo "Installed the Ultimate Vim configuration successfully! Enjoy :-)"
